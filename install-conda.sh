55064ca6bf7fe0acf9dc43499bb069fcb67c12f8
lpy <pengyu.l@foxmail.com>
2024-03-30 17:10:54 +0800

    Update install-conda.sh script to allow specifying user and group

--- install-conda.sh
+++ install-conda.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 INSTALL_PREFIX=/opt/miniconda3
-CONDA_GROUP=${1:-conda}
+CONDA_USER=${1:-conda}
+CONDA_GROUP=${2:-conda}
 
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root!"
     exit 1
 fi
 
-echo "Installing Miniconda3 to $(INSTALL_PREFIX) for group $(CONDA_GROUP)"
-echo "Please ensure that the group $(CONDA_GROUP) exists before running this script."
+echo "Installing Miniconda3 to $(INSTALL_PREFIX) for $(CONDA_USER):$(CONDA_GROUP)"
+echo "Please ensure that the /home/$(CONDA_USER) exists before running this script."
 read -p "Press [Enter] to continue..."
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda.sh
@@ -17,7 +18,7 @@ bash /tmp/miniconda.sh -b -p $(INSTALL_PREFIX) -s
 rm /tmp/miniconda.sh
 
 chmod -R 770 $(INSTALL_PREFIX)
-chgrp -R $(CONDA_GROUP) $(INSTALL_PREFIX)
+chown -R $(CONDA_USER):$(CONDA_GROUP) $(INSTALL_PREFIX)
 
 echo <<EOF >> /opt/conda.sh
 #!/bin/bash
